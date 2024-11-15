6f6735a543d7f468bab3bb8462de864ee570c108
JarzaCode <isaiah.withers@students.coderva.org>
2024-11-15 09:49:50 -0500

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,6 +1,8 @@
 /// blob.js
 document.addEventListener('DOMContentLoaded', function() {
-    var script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/JarzaCode/uBlobeBM/refs/heads/main/main.js';
-    document.body.appendChild(script);
+    if (window.location.href !== 'https://github.com/JarzaCode/uBlobeBM/raw/refs/heads/main/main.html') {
+        var script = document.createElement('script');
+        script.src = 'https://blobby-boi.github.io/uBlobeBM/main.js';
+        document.body.appendChild(script);
+    }
 });
